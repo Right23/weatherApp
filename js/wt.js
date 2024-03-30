4b99c550fdd9de70a2f418cf722187aeb97f3a4b
MHL23 <116019413+Right23@users.noreply.github.com>
2024-03-30 16:55:09 +0700

    Update wt.js

--- js/wt.js
+++ js/wt.js
@@ -8,7 +8,7 @@ const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 
 const sunrise = document.querySelector('.sunrise');
-const sunrset = document.querySelector('.sunrset');
+const sunset = document.querySelector('.sunset');
 const humidity = document.querySelector('.humidity');
 const windSpeed = document.querySelector('.wind-speed');
 
@@ -23,6 +23,11 @@ searchInput.addEventListener('change', (e)=>{
             weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
             temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
             weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;
+            humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
         })
 });
 
